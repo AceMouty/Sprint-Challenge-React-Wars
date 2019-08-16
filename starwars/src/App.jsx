98e91233ea70635f835928498d9f6bd34e0e8bef
Kyle Guerrero <kyleguerrero85@gmail.com>
2019-08-16 10:40:57 -0500

    Have setup state useEfftect and axios call along with a loading message to display while we wait for the axios call to resolve

---
+++ starwars/src/App.jsx
@@ -0,0 +1,37 @@
+
+// Import dependencies and lib's
+import React, {useState, useEffect} from 'react';
+import axios from 'axios';
+
+// Importing styles
+import './App.css';
+
+const App = () => {
+
+  //Declaring state to be used for the app
+	const [characters, setCharacters] = useState();
+
+	// calling use effect and syncing to any change in state for this component.
+	// On first render make an axios call and set state, this will cause a rerender in the coponent with updated state.
+	useEffect(() => {
+		axios('https://swapi.co/api/people/')
+		.then(res => {
+
+		// console.log(res.data.results);
+		const apiData = res.data.results
+		setCharacters(apiData);
+	})
+	}, []);
+
+	// If characters does NOT evaluate to true display a loading text
+	if(!characters) return <h1 className="loadingText">Loading...</h1>
+
+	// Once state has updated with a new value we will display this content since or conditional logic is no longer met.
+  return (
+    <div className="App">
+      <h1 className="Header">React Wars</h1>
+    </div>
+  );
+}
+
+export default App;
